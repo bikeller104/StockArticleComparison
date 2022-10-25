2c78489e2021264a471e73fd82c75fd69b764b55
Ari N <an-nielson@hotmail.com>
2022-10-24 20:43:48 -0600

    updated html & css for styling

---
+++ script.js
@@ -0,0 +1,28 @@
+const searchFrom = document.querySelector(".search");
+const input = document.querySelector(".input");
+const articleList = document.querySelector(".list");
+
+searchFrom.addEventListener("submit", searchTopic);
+
+function searchTopic(e) {
+  e.preventDefault();
+  let topic = input.value;
+  let url = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${topic}&limit=25&apikey=Y7EZGMG4B18PRI2S`;
+
+  fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.feed.forEach((feed) => {
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        a.setAttribute("href", feed.url);
+        a.setAttribute("target", "_blank");
+        a.textContent = feed.title;
+        li.appendChild(a);
+        articleList.appendChild(li);
+      });
+    });
+}
