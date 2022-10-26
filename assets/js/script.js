8351250cd48b9d7769a4504d2750066c7b7e381f
Ari N <an-nielson@hotmail.com>
2022-10-25 21:13:27 -0600

    re-structured folders, added dark-light mode, edits to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,19 @@ function searchTopic(e) {
 
 }
 
+// Dark Light Mode Function
+var themeSwitcher = document.querySelector("#theme-switcher");
+var page = document.querySelector(".page");
+
+var modeDefault = "dark";
+
+
+themeSwitcher.addEventListener("click", function() {  
+  if (modeDefault === "dark") {
+    modeDefault = "light";
+    page.setAttribute("class", "light");
+  } else {
+    modeDefault = "dark";
+    page.setAttribute("class", "dark");
+  }
+});
