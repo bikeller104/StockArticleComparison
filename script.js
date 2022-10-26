950731e01462a89c408c9514e75f689fa80727ec
eljayman <lelandoj@gmail.com>
2022-10-25 20:34:07 -0600

    added date range and targeted the search with those constraints

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const searchFrom = document.querySelector(".search");
 const input = document.querySelector(".input");
 const articleList = document.querySelector(".list");
-
-searchFrom.addEventListener("submit", searchTopic);
+const datePickerStart = document.getElementById("start-date");
+const datePickerSubBtn = document.getElementById("date-start-btn");
+const start = datepicker(".start", {
+  id: 1,
+  formatter: (input, date, instance) => {
+    const value = date.toLocaleDateString("sv").replaceAll("-", "");
+    input.value = value;
+  },
+});
+const end = datepicker(".end", {
+  id: 1,
+  formatter: (input, date, instance) => {
+    const value = date.toLocaleDateString("sv").replaceAll("-", "");
+    input.value = value;
+  },
+});
 
 function searchTopic(e) {
   e.preventDefault();
+  const dateStartValue = `${document.querySelector(".start").value}T0000`;
+  const dateEndValue = `${document.querySelector(".end").value}T0000`;
   let topic = input.value;
-  let url = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${topic}&limit=25&apikey=Y7EZGMG4B18PRI2S`;
+  let url = `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${topic}&time_from=${dateStartValue}&time_to=${dateEndValue}&apikey=Y7EZGMG4B18PRI2S`;
 
   fetch(url)
     .then((res) => {
@@ -25,6 +41,6 @@ function searchTopic(e) {
         articleList.appendChild(li);
       });
     });
-
 }
 
+searchFrom.addEventListener("submit", searchTopic);
