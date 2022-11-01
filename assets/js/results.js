cd6896b417617a534b390d9dbdc79c3a3b15071f
Brent Keller <bikeller104@gmail.com>
2022-10-31 20:46:52 -0600

    added identifier for current articles

--- assets/js/results.js
+++ assets/js/results.js
@@ -14,6 +14,7 @@ themeSwitcher.addEventListener("click", function () {
 
 var SearchData;
 var articleList = document.getElementById("article-list");
+var articleListPresent = $("#present-articleList")
 
 
 
